f46f5275601ff691cb65ee504fcefed0e9d4d278
Brian Dillmann <dillmann.brian@gmail.com>
2016-06-13 08:27:01 -0400

    set up everything

---
+++ lib/config.py
@@ -0,0 +1,25 @@
+import os
+import sqlite3
+
+class Config:
+
+    def __init__(self, config_file):
+        if not config_file:
+            raise Exception('No configuration file name provided.')
+        self.conn = sqlite3.connect(config_file)
+
+    def get(self, cfg_id):
+        """ Fetches a configuration value by id if config item exists. """
+
+        if not cfg_id:
+            raise Exception("No configuration item id given.")
+
+        query = "SELECT val FROM Configuration WHERE id = '%s'" % cfg_id
+        c = self.conn.cursor(query)
+        c.execute()
+
+        row = c.fetchone()
+        if not row:
+            raise Exception("Invalid Configuration item: '%s'" % cfg_id)
+        else:
+            return row[0]
---
+++ tests/config_test.py
@@ -0,0 +1,5 @@
+from lib.config import Config
+cg = None
+
+def test_init():
+    cf = Config('config.db')
