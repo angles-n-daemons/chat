93adefc9a6338ec5a5953feba70625381ad8ea2a
Brian Dillmann <brian.dillman@annalect.com>
2016-06-20 18:24:54 -0400

    trying to figure out sublime bug

--- lib/auth.py
+++ lib/auth.py
@@ -1,4 +1,5 @@
 import os
 
 class Auth:
-    def __init__()
+    def __init__(self):
+        self.conn = 
--- tests/config_test.py
+++ tests/config_test.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from ..lib.config import Config
 
 def test_config():
@@ -7,3 +8,9 @@ def test_config():
         cfg = Config('')
     val = cfg.get('socket-key')
 
+def test_config_check_create():
+    cfg = Config('config.db')
+    os.remove('config.db')
+    assert cfg.check_config_table() == False
+    cfg.create_config_table()
+    assert cfg.check_config_table() == True
